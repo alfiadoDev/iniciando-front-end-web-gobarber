067dd2b06fbb827bfe52ae8217dfea0d4bbbb51b
Alfiado Constantino <alfiado02@gmail.com>
2020-11-16 12:57:14 +0200

    teste de login. ainda com erros

--- src/__tests__/pages/Signin.spec.tsx
+++ src/__tests__/pages/Signin.spec.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Signin from '../../pages/Signin';
 
+const mockedHistory = jest.fn();
+
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistory,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('Signin Page', () => {
   it('it should be able to signin', () => {
-    const { debug } = render(<Signin />);
+    const { getByPlaceholderText, getByText } = render(<Signin />);
 
-    debug();
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordlField = getByPlaceholderText('Senha');
+    const buttonTextElement = getByText('Enviar');
+
+    fireEvent.change(emailField, { target: { value: 'alfiado@alfiado.com' } });
+    fireEvent.change(passwordlField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonTextElement);
+
+    expect(mockedHistory).toHaveBeenCalledWith('/');
   });
 });
